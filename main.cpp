b7c029751d3fcf718661abc1ec5c999479c317dc
Robert Pogue <robert@epogue.com>
2015-08-24 12:39:50 -0500

    Add compiling SDL2 project.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,44 @@
-#include "Graphics.h"
+#include <SDL.h>
+#include <stdio.h>
+
+// screen dimensions
+const int SCREEN_WIDTH = 640; // px
+const int SCREEN_HEIGHT = 480; // px
+
+int main(int argc, char* args[]) {
+	SDL_Window* window = nullptr;
+	SDL_Surface* screen = nullptr;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+		printf("SDL could not initialize. SDL_Error: %s\n", SDL_GetError());
+	} else {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        } else {
+            //Get window surface
+            screen = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screen, NULL, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Wait two seconds
+            SDL_Delay( 2000 );
+        }
+    }
+	//Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
+}
+/*#include "Graphics.h"
 #include "Input.h"
 #include "Level.h"
 #include "Time.h"
@@ -48,3 +88,4 @@ int main(int argc, char* args[]) {
     Graphics::shutDown();
     return 0;
 }
+*/
