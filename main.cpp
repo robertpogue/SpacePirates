f8277ddfad66ffeb84012083b235a99c172bab7c
Robert Pogue <robert@stadia3d.com>
2016-02-23 21:53:34 -0500

    display image in window.

--- main.cpp
+++ main.cpp
@@ -1,42 +1,43 @@
 #include <SDL.h>
-#include <stdio.h>
+#include <exception>
+#include <string>
+#include <assert.h>
 
-// screen dimensions
-const int SCREEN_WIDTH = 640; // px
-const int SCREEN_HEIGHT = 480; // px
+int main(int argc, char ** argv) {
+    try {
+        int result = SDL_Init(SDL_INIT_VIDEO);
+        assert(result);
 
-int main(int argc, char* args[]) {
-	SDL_Window* window = nullptr;
-	SDL_Surface* screen = nullptr;
-	if (SDL_Init(SDL_INIT_VIDEO) < 0 ) {
-		printf("SDL could not initialize. SDL_Error: %s\n", SDL_GetError());
-	} else {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        } else {
-            //Get window surface
-            screen = SDL_GetWindowSurface( window );
+        SDL_Window* window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+        assert(window);
 
-            //Fill the surface white
-            SDL_FillRect( screen, NULL, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        assert(renderer);
 
-            //Wait two seconds
-            SDL_Delay( 2000 );
+        std::string imagePath = "hello.bmp";
+        SDL_Surface* bmp = SDL_LoadBMP(imagePath.c_str());
+        assert(bmp);
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, bmp);
+        SDL_FreeSurface(bmp);
+        assert(texture);
+
+        // rendering loop
+        for(int i = 0; i < 3; ++i) {
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, texture, NULL, NULL);
+            SDL_RenderPresent(renderer);
+            SDL_Delay(1000);
         }
+
+        SDL_DestroyTexture(texture);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 0;
+    } catch(std::exception) {
+        return 1;
     }
-	//Destroy window
-    SDL_DestroyWindow( window );
-
-    //Quit SDL subsystems
-    SDL_Quit();
-
-    return 0;
 }
 /*#include "Graphics.h"
 #include "Input.h"
@@ -53,6 +54,10 @@ int main(int argc, char* args[]) {
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+
+// screen dimensions
+const int SCREEN_WIDTH = 640; // px
+const int SCREEN_HEIGHT = 480; // px
 
 int main(int argc, char* args[]) {
     try {
