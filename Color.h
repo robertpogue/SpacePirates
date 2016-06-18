c17d558db8be656903c1bfbb509e96ec1c4e93ab
Alex Pogue <alex@epogue.com>
2016-06-18 17:24:12 -0500

    Remove extern from overloaded equals

--- Color.h
+++ Color.h
@@ -6,6 +6,6 @@ struct Color {
     float rNorm() {return r/255.0f;} // normalized to 0-1 color range 
     float gNorm() {return g/255.0f;}
     float bNorm() {return b/255.0f;}
-    friend extern bool operator==(const Color& c1, const Color& c2);
-    friend extern bool operator!=(const Color& c1, const Color& c2);
+    friend bool operator==(const Color& c1, const Color& c2);
+    friend bool operator!=(const Color& c1, const Color& c2);
 };
